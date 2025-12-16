65c45a9a32eed7765ea044c59394c317e9999c3c
codexdei <jlacostc@gmail.com>
2025-12-15 21:37:25 -0500

    se retorna interceptor false para probar accesos

--- springboot_interceptor/src/main/java/com/codexdei/springboot/app/interceptor/springboot_interceptor/MvcConfig.java
+++ springboot_interceptor/src/main/java/com/codexdei/springboot/app/interceptor/springboot_interceptor/MvcConfig.java
@@ -1,14 +1,30 @@
 package com.codexdei.springboot.app.interceptor.springboot_interceptor;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class MvcConfig implements WebMvcConfigurer{
+
+    @Autowired
+    @Qualifier("timeInterceptor")
+    private HandlerInterceptor timeInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        // TODO Auto-generated method stub
-        WebMvcConfigurer.super.addInterceptors(registry);
+
+        //addPathPatterns permite excluir con que metodos del controlador se generaran los mensajes del logger en la consola o terminal
+        // registry.addInterceptor(timeInterceptor).excludePathPatterns("/app/bar","/app/baz");
+        //habilita todos los metodos de la ruta /app
+        //registry.addInterceptor(timeInterceptor).addPathPatterns("/app/**");
+        //addPathPatterns permite agregar con que metodos del controlador se generaran los mensajes del logger en la consola o terminal
+        //registry.addInterceptor(timeInterceptor).excludePathPatterns("/app/bar","/app/baz");
+        //addPathPatterns permite agregar con que metodos del controlador se generaran los mensajes del logger en la consola o terminal
+        registry.addInterceptor(timeInterceptor).addPathPatterns("/app/bar","/app/baz");
     }
 
     
--- springboot_interceptor/src/main/java/com/codexdei/springboot/app/interceptor/springboot_interceptor/interceptors/LoadingTimeInterceptor.java
+++ springboot_interceptor/src/main/java/com/codexdei/springboot/app/interceptor/springboot_interceptor/interceptors/LoadingTimeInterceptor.java
@@ -1,16 +1,23 @@
 package com.codexdei.springboot.app.interceptor.springboot_interceptor.interceptors;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 import org.jspecify.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-@Component
+@Component("timeInterceptor")
 public class LoadingTimeInterceptor implements HandlerInterceptor {
 
     private static final Logger logger = LoggerFactory.getLogger(LoadingTimeInterceptor.class);
@@ -19,15 +26,42 @@ public class LoadingTimeInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
 
-        logger.info("LoadingTimeInterceptor: pretHandle: entrando......");
-        return true;
+        HandlerMethod controller = ((HandlerMethod) handler);
+
+        logger.info("LoadingTimeInterceptor: pretHandle: entrando......" + controller.getMethod().getName());
+
+        Long start = System.currentTimeMillis();
+        request.setAttribute("start", start);
+
+        Random random = new Random();
+        int delay = random.nextInt(500);
+        Thread.sleep(delay);
+
+        Map<String,String> json = new HashMap<>();
+        json.put("error", "No tienes acceso a esta pagina");
+        json.put("date", new Date().toString());
+
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonString = mapper.writeValueAsString(json);
+        response.setContentType("application/json");
+        response.setStatus(401);
+        response.getWriter().write(jsonString);;
+
+
+        return false;
+        //return true;
     }
     
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
             @Nullable ModelAndView modelAndView) throws Exception {
 
-        logger.info("LoadingTimeInterceptor: postHandle: saliendo......");
+        Long end = System.currentTimeMillis();
+        Long start = (Long) request.getAttribute("start");
+        Long result = end - start;
+
+        logger.info("Tiempo transcurrido: " + result + " milisegundos!");
+        logger.info("LoadingTimeInterceptor: postHandle: saliendo......" + ((HandlerMethod) handler).getMethod().getName());
 
     }
 
